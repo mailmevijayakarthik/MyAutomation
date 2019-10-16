8fe6c3eeda8f4aa6f2a580dcfa47c6825518f7b8
Vijayakarthikeyan Arul <vijayakarthikeyan.arul@heb.com>
2019-10-16 14:53:23 -0500

    Added sample script to add cucumber based timeout parameter in the step definition to fail the script if it exceeds the specific timeout value

--- src/test/java/com/testing/stepdefinitions/RESTAssuredStepdef.java
+++ src/test/java/com/testing/stepdefinitions/RESTAssuredStepdef.java
@@ -38,7 +38,64 @@ public class RESTAssuredStepdef extends BaseRestTest{
 	}
 
 	
-	@Given("Register a new customer")
+	@Given(value="^Register a new customer$", timeout=200)
+	public void registeration_a_new_customer() {
+	    
+RestAssured.baseURI=prop.getProperty("Endpoint.baseurl");
+		
+		// Request Object 
+		RequestSpecification httprequest=RestAssured.given();
+		
+		/*
+		 * // Basic Authentication with Username and password PreemptiveBasicAuthScheme
+		 * auth = new PreemptiveBasicAuthScheme(); auth.setUserName("user");
+		 * auth.setPassword("fdfd");
+		 * 
+		 * RestAssured.authentication=auth;
+		 */
+		
+		
+		// Request Parameters 
+		JSONObject requestParams=new JSONObject();
+		
+		requestParams.put("FirstName","JohnXYZ");
+		  requestParams.put("LastName","XYZJohn");
+		  requestParams.put("UserName","JohnXYZ");
+		  requestParams.put("Password","JohnXYZxyx");
+		  requestParams.put("Email","JohnXYZ@gmail.com");
+		  
+		  httprequest.header("Content-Type","application/json");
+		  httprequest.body(requestParams.toJSONString()); // attach above data to the request
+				
+		// Response Object 
+		Response response = httprequest.request(Method.POST, "customer/register");
+		
+		// print response 
+		
+				String Responsebody= response.getBody().asString();
+				System.out.println("Response Body :"+ Responsebody);
+				int statuscode = response.statusCode();
+				System.out.println("StatusCode :"+ statuscode);
+				
+			
+				
+		// Verification checkpoints
+		/*
+		 * Verify the Header response Content-Encoding is gzip
+		 */
+				
+		 String contentheader= response.header("Content-Encoding");
+		 Assert.assertEquals(contentheader, "gzip");
+		 
+		 // To verify any specific object in the response 
+		 
+			String errorreason= response.jsonPath().get("fault");
+			Assert.assertEquals(errorreason, "FAULT_USER_ALREADY_EXISTS");
+		 
+	
+	}
+	
+	//@Given("Register a new customer")
 	public void register_a_new_customer() {
 	    
 RestAssured.baseURI=prop.getProperty("Endpoint.baseurl");
